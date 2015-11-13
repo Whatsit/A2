2299a9eb381e6cf8b716471d93c87991f15eb327
Whatsit <Ryan@Apollo.local>
2015-11-13 15:46:10 -0800

    Removed old code

--- A2/A2/main.cpp
+++ A2/A2/main.cpp
@@ -44,23 +44,6 @@ int main(int argc, const char * argv[]) {
             cout << "command not recognized" << endl;
         }
     }
-    /*
-    cout << "ENTER NAME: ";
-    cin >> name;
-    cout << endl << "Hello " << name;
-    
-    Character user1(name);
-    
-    cout << endl << "Hello " << user1.getName();
-    
-    cout << endl << "ENTER NEW USER: ";
-    cin >> name;
-    
-    Character user2("default");
-    user2.setName(name);
-    cout << endl << "Thank you " << user1.getName() << " for letting " << user2.getName() << " register" << endl << "Have a nice day" << endl << endl;
-    */
-    
     
     return 0;
 }
